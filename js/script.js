74788c0f41db4c62919bbb357a257a5601252bba
Bekbolsyn <32035749+Bekbolsyn@users.noreply.github.com>
2019-11-30 23:51:03 +0700

    site 2.0
    new site

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ $(".detail_services .item").click(function() {
 	$(".des_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
+	$(document).ready(function(){
+    $("nav").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
